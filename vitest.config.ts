9f109901b9ff2cb96362101e926443dbacf4d95a
Diakite Khalidou <diakitekhalidou1@gmail.com>
2026-07-13 19:33:24 +0200

    Prepare frontend Jenkins pipeline

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: './junit.xml'
+    },
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
